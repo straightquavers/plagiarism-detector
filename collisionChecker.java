e674bfa53c524af6d4b17721ef5514237b956ccf
Phoebe Owen <x4o25@mercia.keele.ac.uk>
2023-01-27 16:49:11 +0000

    added pseudocode to collision checker

--- collisionChecker.java
+++ collisionChecker.java
@@ -1,3 +1,25 @@
 public class collisionChecker {
-    
+// returns areas of collisions as references to locations
+// returns distances between tokens
+//
+
+    //nlp
+    //- Comments being copied.
+    // - Checking the whole code through these libraries;
+    // - Identifies redundant code added.
+    // - Code being split up or merged.
+
+//     - Author name not matching submission name - student ID rather than name potentially. Search the file for potential student IDs and compare against folder or file name.
+
+// - Formatting
+//     - Whitespaces (matching another file)
+//         - Too many blank lines
+//         - Spaces at the start and end of lines.
+//     - Inconsistent formatting (brackets on the same line and brackets on different lines in the same file).
+
+// - Tokens
+//     - The same code rearranged to different lines.
+//     - The same code rearranged to different classes/files.
+//     - Variable names being changed but output the same.
+//     - Method calls being changed to using the actual method code and vice versa.
 }

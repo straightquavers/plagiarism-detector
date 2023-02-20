e0d1ccba44c120b97023e0ab7fd1779b74dd7b04
straightquavers <phee3212@gmail.com>
2023-02-20 09:41:08 +0000

    Improved plan for file processing

--- fileProcessor.java
+++ fileProcessor.java
@@ -1,6 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class fileProcessor {
-    //read file
-    //put file in data structure
-    //tokenize method
-    //put tokens in data structure
+    // search for files in directory
+    // unzip files
+    // input file names into structure
+    // iterate reading files through the structure of names
+    // put files in different data structures
+    // tokenize method
+    // put tokens in data structure
+
+    public static void read(String[] args) {
+        // iterate through
+        // for (int i = 0; i < filenames.length(); i++) {
+        // try {
+        // filename = filenames[i] + "txt";
+        // File myObj = new File(filename);
+        // Scanner myReader = new Scanner(myObj);
+        // while (myReader.hasNextLine()) {
+        // String data = myReader.nextLine();
+        // System.out.println(data);
+        // }
+        // myReader.close();
+        // } catch (FileNotFoundException e) {
+        // System.out.println("An error occurred.");
+        // e.printStackTrace();
+        // }
+        // }
+
+    }
+
 }

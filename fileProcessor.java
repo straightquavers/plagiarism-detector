e6a4279dc846503622dc70dfbf9839fcd4d28327
straightquavers <phee3212@gmail.com>
2023-02-21 15:13:32 +0000

    Improvements to file reading

--- fileProcessor.java
+++ fileProcessor.java
@@ -1,20 +1,36 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class fileProcessor {
     // search for files in directory
     // unzip files
     // input file names into structure
     // iterate reading files through the structure of names
-    // put files in different data structures
+    // put files in different data structures, strip escape characters from
+    // comments, potentially put comments in different data structure
     // tokenize method
     // put tokens in data structure
 
+    // constructor that takes filedirectory
+    public fileProcessor(String directory) {
+        ArrayList<String> filenames = new ArrayList<String>();
+        //iterate through files
+        filenames = 
+    }
+
     public static void read(String[] args) {
+
         // iterate through
+        ArrayList<ArrayList<File>> files = new ArrayList<ArrayList<File>>();
+        // for (int i = 0; i < filenames; i++) {
+        // ArrayList<File> currentFile = new ArrayList<File>();
+        // files.add(currentFile);
+        // }
+
+        // use files.get(0) etc to get at each list
         // for (int i = 0; i < filenames.length(); i++) {
         // try {
-        // filename = filenames[i] + "txt";
+        // filename = filenames[i] + ".txt";
         // File myObj = new File(filename);
         // Scanner myReader = new Scanner(myObj);
         // while (myReader.hasNextLine()) {
